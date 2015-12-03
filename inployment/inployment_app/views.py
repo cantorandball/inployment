f1071ed09464def539bf117ca00cadf54547ab18
josh <joshsmith2@gmail.com>
2015-12-03 23:34:38 +0000

    Got porpy up. Essentially, added app to settings.py, made home template, updated views to render it. Still not really a website.

--- inployment/inployment_app/views.py
+++ inployment/inployment_app/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Test view from Django tutorial: https://docs.djangoproject.com/en/1.9/intro/tutorial01/
 
 def index(request):
-    return HttpResponse("This is a little tiny acorn of a website. Make it grow.")
+    return render(request, 'home.html')
