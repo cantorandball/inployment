bc3e276d03772fd5fe6fa3276b023de8a0412358
josh <joshsmith2@gmail.com>
2015-12-04 00:42:46 +0000

    Got emails saving to database. Going to bed.

--- inployment/inployment_app/views.py
+++ inployment/inployment_app/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from inployment.inployment_app.models import PotentialUser
 
 # Test view from Django tutorial: https://docs.djangoproject.com/en/1.9/intro/tutorial01/
 
 def index(request):
+    # Save email
+    if request.method == 'POST':
+        curious_email = request.POST['curious_email']
+        PotentialUser.objects.create(email_address=curious_email)
+
     return render(request, 'home.html')
