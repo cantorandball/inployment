11f392536007ec9ff88464df5f082abab1f59d98
josh <joshsmith2@gmail.com>
2015-12-07 14:15:15 +0000

    CherryPy server working, without authentication. Had to write a .sh script intead of using the Dockerfile, which was a bit iffy tbh

--- inployment/wsgi.py
+++ inployment/wsgi.py
@@ -8,8 +8,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "inployment.settings")
+# Get inloyment onto sys.path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 application = get_wsgi_application()
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pip install -r requirements.txt
+pip list
+python server.py
