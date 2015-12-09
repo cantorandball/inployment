dd2b2bdfa83a2aba10f05c63fe63f78be8a3ca52
josh <joshsmith2@gmail.com>
2015-12-09 23:00:03 +0000

    Business form is wired up. Now need to do others, then style.

---
+++ inployment-django/inployment/inployment_app/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+
+class BusinessForm(forms.Form):
+    business_name = forms.CharField(
+        max_length=100,
+        label='',
+        widget=forms.TextInput(attrs={'placeholder': 'Business name',
+                                      'class': 'form-control'})
+    )
+    business_email = forms.EmailField(
+        required=True,
+        label='',
+        widget=forms.TextInput(attrs={'placeholder': 'Email address',
+                                      'class': 'form-control'})
+    )
+
+class CandidateForm(forms.Form):
+    candidate_email = forms.EmailField(required=True)
+
+class InterestedForm(forms.Form):
+    interested_email = forms.EmailField(required=True)
--- inployment-django/inployment/inployment_app/views.py
+++ inployment-django/inployment/inployment_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from models import *
+from forms import *
 
 # Test view from Django tutorial: https://docs.djangoproject.com/en/1.9/intro/tutorial01/
 
@@ -8,10 +9,14 @@ def index(request):
     # Save email
     if request.method == 'POST':
         if 'business' in request.POST:
-            business_name = request.POST['business_name']
-            business_email = request.POST['business_email']
-            Business.objects.create(name=business_name,
-                                    email_address=business_email)
+            business_form = BusinessForm(request.POST)
+            if business_form.is_valid():
+                business_name = business_form.cleaned_data['business_name']
+                business_email = business_form.cleaned_data['business_email']
+                Business.objects.create(name=business_name,
+                                        email_address=business_email)
+                return redirect('thanks/')
+
         elif 'candidate' in request.POST:
             candidate_email = request.POST['candidate_email']
             Candidate.objects.create(email_address=candidate_email)
@@ -19,10 +24,11 @@ def index(request):
             interested_email = request.POST['interested_email']
             InterestedParty.objects.create(email_address=interested_email)
 
-        out = redirect('thanks/')
     else:
-        out = render(request, 'home.html')
-    return out
+        business_form = BusinessForm()
+
+    return render(request, 'home.html', {'business_form':business_form})
+
 
 def thanks(request):
     return render(request, 'thanks.html')
