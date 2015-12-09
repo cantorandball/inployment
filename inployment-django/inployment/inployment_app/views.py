e0d2a916c8f8c043858ff34f0ffbd9a69b539e68
josh <joshsmith2@gmail.com>
2015-12-09 15:05:05 +0000

    Added redirect after post

--- inployment-django/inployment/inployment_app/views.py
+++ inployment-django/inployment/inployment_app/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from inployment.inployment_app.models import *
+from models import *
 
 # Test view from Django tutorial: https://docs.djangoproject.com/en/1.9/intro/tutorial01/
 
@@ -17,7 +17,10 @@ def index(request):
             interested_email = request.POST['interested_email']
             InterestedParty.objects.create(email_address=interested_email)
 
-    return render(request, 'home.html')
+        out = redirect('/inployment/thanks/')
+    else:
+        out = render(request, 'home.html')
+    return out
 
 def thanks(request):
     return render(request, 'thanks.html')
