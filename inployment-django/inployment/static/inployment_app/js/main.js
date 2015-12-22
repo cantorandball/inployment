08ed445a9bed12ca5c03e06263daa9ba851cc0b0
josh <joshsmith2@gmail.com>
2015-12-22 11:03:33 +0000

    Slidy boxes really working now. Click to open or close. Input stopping propogation

--- inployment-django/inployment/static/inployment_app/js/main.js
+++ inployment-django/inployment/static/inployment_app/js/main.js
@@ -1,7 +1,21 @@
 $( document).ready(function() {
+    $(".email-box").bind({
+        /*
+        mouseenter: function () {
+            $(this).children(".blinds").show("blind");
+        },
+        mouseleave: function () {
+            $(this).children(".blinds").hide("blind");
+        }
+         */
 
-    $(".email-box").click(function () {
-        $(this).children(".blinds").toggle("blind", {"direction": "down"});
+        click: function () {
+            $(this).children(".blinds").toggle("blind");
+        }
     });
 
+    // Stop the div from closing when an input element is clicked
+    $(".form-control").click(function(e){
+        e.stopPropagation();
+    });
 });
