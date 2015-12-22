1c5bc9b8cd90e19879734b3d34a5c6c4d821d785
josh <joshsmith2@gmail.com>
2015-12-22 11:07:13 +0000

    Changed placeholder to title on forms

--- inployment-django/inployment/inployment_app/forms.py
+++ inployment-django/inployment/inployment_app/forms.py
@@ -3,29 +3,25 @@ from django import forms
 class BusinessForm(forms.Form):
     business_name = forms.CharField(
         max_length=100,
-        label='',
-        widget=forms.TextInput(attrs={'placeholder': 'Business name',
-                                      'class': 'form-control'})
+        label='Company Name',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
     )
     business_email = forms.EmailField(
         required=True,
-        label='',
-        widget=forms.TextInput(attrs={'placeholder': 'Email address',
-                                      'class': 'form-control'})
+        label='Email address',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
     )
 
 class CandidateForm(forms.Form):
     candidate_email = forms.EmailField(
         required=True,
-        label='',
-        widget=forms.TextInput(attrs={'placeholder': 'Email address',
-                                      'class': 'form-control'})
+        label='Email address',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
     )
 
 class InterestedForm(forms.Form):
     interested_email = forms.EmailField(
         required=True,
-        label='',
-        widget=forms.TextInput(attrs={'placeholder': 'Email address',
-                                      'class': 'form-control'})
+        label='Email address',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
     )
