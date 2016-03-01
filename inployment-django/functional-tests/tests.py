3a344cf854f439d9e73431fcd0905fa26d6cad33
josh <joshsmith2@gmail.com>
2016-03-01 19:53:14 +0000

    test for label working

--- inployment-django/functional-tests/tests.py
+++ inployment-django/functional-tests/tests.py
@@ -18,5 +18,21 @@ class FunctionalTest(StaticLiveServerTestCase):
     def test_title_exists(self):
         self.browser.get("localhost:8000")
 
-        # Check Inployment in the title
+        # Check Inployment in the title and h1
         self.assertIn("Inployment", self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertEquals("inployment", header_text.lower())
+
+    def test_candidate_journey_is_correct(self):
+        self.browser.get("localhost:8000")
+
+        #Candidate logs in. Input box is hidden because reasons
+        input_id = "id_candidate_email"
+        input_box = self.browser.find_element_by_id(input_id)
+        input_label = self.browser.find_element_by_xpath(
+            "//label[@for='" + input_id + "']"
+        )
+
+        self.assertEquals("Email address:", input_label.text)
+
+
